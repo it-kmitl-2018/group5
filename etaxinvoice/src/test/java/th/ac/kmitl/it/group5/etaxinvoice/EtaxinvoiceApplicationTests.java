e5b83f9cb48214fa5623f9445d0909514f879a97
pattasing <pattarawadee-s@hotmail.com>
2018-02-18 23:41:55 +0700

    Initial project with maven and spring framework.

---
+++ etaxinvoice/src/test/java/th/ac/kmitl/it/group5/etaxinvoice/EtaxinvoiceApplicationTests.java
@@ -0,0 +1,17 @@
+package th.ac.kmitl.it.group5.etaxinvoice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtaxinvoiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		//To do.
+	}
+
+}
