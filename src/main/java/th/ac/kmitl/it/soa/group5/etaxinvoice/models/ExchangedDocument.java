990ac60961449756a549223ed1fb50f07d27a906
Benjarat Chantaprasert <sakjura@gmail.com>
2018-03-06 09:23:36 +0700

    update exchangedDocument model using lombok with unit test

--- src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ExchangedDocument.java
+++ src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ExchangedDocument.java
@@ -1,7 +1,9 @@
 package th.ac.kmitl.it.soa.group5.etaxinvoice.models;
 
+import lombok.Data;
 import java.sql.Timestamp;
 
+@Data
 public class ExchangedDocument {
 
     public String id;
@@ -10,46 +12,6 @@ public class ExchangedDocument {
     public Timestamp issueDateTime;
     public Timestamp createDateTime;
 
-    public ExchangedDocument() {
-    }
+    public ExchangedDocument() { }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTypeCode() {
-        return typeCode;
-    }
-
-    public void setTypeCode(String typeCode) {
-        this.typeCode = typeCode;
-    }
-
-    public Timestamp getIssueDateTime() {
-        return issueDateTime;
-    }
-
-    public void setIssueDateTime(Timestamp issueDateTime) {
-        this.issueDateTime = issueDateTime;
-    }
-
-    public Timestamp getCreateDateTime() {
-        return createDateTime;
-    }
-
-    public void setCreateDateTime(Timestamp createDateTime) {
-        this.createDateTime = createDateTime;
-    }
 }
--- src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ExchangedDocumentTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ExchangedDocumentTest.java
@@ -3,19 +3,15 @@ package th.ac.kmitl.it.soa.group5.etaxinvoice.models;
 import org.junit.Test;
 
 import java.sql.Timestamp;
-
 import static org.junit.Assert.*;
 
 public class ExchangedDocumentTest {
-
 
     private String id = "RDTIV0575526000058001";
     private String name = "ใบกำกับภาษี";
     private String typeCode = "388";
     private Timestamp issueDateTime = new Timestamp(System.currentTimeMillis());
     private Timestamp createDateTime = new Timestamp(System.currentTimeMillis());
-
-
 
     @Test
     public void getValueFromExchangedDocumentModel(){
