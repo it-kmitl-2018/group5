22cabf6478d209548f6b988e9c9b244ba30dd70a
Benjarat Chantaprasert <sakjura@gmail.com>
2018-03-14 17:17:18 +0700

    Delete annotation xml in model

--- src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ETaxInvoiceHeader.java
+++ src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/ETaxInvoiceHeader.java
@@ -1,26 +1,14 @@
 package th.ac.kmitl.it.soa.group5.etaxinvoice.models;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.experimental.Builder;
 import java.sql.Timestamp;
 
-@JacksonXmlRootElement(localName = "rsm:ExchangedDocument")
 @Builder(builderClassName = "Builder")
 public class ETaxInvoiceHeader {
 
-    @JacksonXmlProperty(localName = "ram:ID")
     public String id;
-
-    @JacksonXmlProperty(localName = "ram:Name")
     public String name;
-
-    @JacksonXmlProperty(localName = "TypeCode")
     public String typeCode;
-
-    @JacksonXmlProperty(localName = "IssueDateTime")
     public Timestamp issueDateTime;
-
-    @JacksonXmlProperty(localName = "CreateDateTime")
     public Timestamp createDateTime;
 }
