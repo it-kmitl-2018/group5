33bfc47e35d0502201ebd9444a851b5caa94e99b
Benjarat Chantaprasert <sakjura@gmail.com>
2018-03-14 18:27:53 +0700

    Change HTTP method to get

--- src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/controllers/ETaxInvoiceHeaderController.java
+++ src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/controllers/ETaxInvoiceHeaderController.java
@@ -1,16 +1,14 @@
 package th.ac.kmitl.it.soa.group5.etaxinvoice.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import th.ac.kmitl.it.soa.group5.etaxinvoice.forms.ETaxInvoiceHeaderForm;
-import th.ac.kmitl.it.soa.group5.etaxinvoice.models.ETaxInvoiceHeader;
-import java.sql.Timestamp;
 
 @RestController
 public class ETaxInvoiceHeaderController {
 
-    @PostMapping("/tax_invoice_header")
+    @GetMapping("/tax_invoice_header")
     public ETaxInvoiceHeaderForm eTaxInvoiceHeader(@RequestBody ETaxInvoiceHeaderForm eTaxInvoiceHeaderForm) {
         return eTaxInvoiceHeaderForm;
     }
