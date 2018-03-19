441dcc5bc0a8ddd47a1af8470910f2d7c842ef51
mild supanat <mildsupanat@hotmail.com>
2018-03-19 20:20:30 +0700

    Add TradeSettlementTests

--- src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/TradeSettlementMonetarySummaryControllerTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/TradeSettlementMonetarySummaryControllerTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
