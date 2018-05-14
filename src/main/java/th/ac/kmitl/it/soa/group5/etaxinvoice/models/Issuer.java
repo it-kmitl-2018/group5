2133ce6c4d16b8658e7548cf7ed307b97fc73072
Natcha <58070069@kmitl.ac.th>
2018-05-14 19:35:43 +0700

    Rename variables

--- src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/Issuer.java
+++ src/main/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/Issuer.java
@@ -5,7 +5,7 @@ import lombok.experimental.Builder;
 @Builder(builderClassName = "Builder")
 public class Issuer {
     public String issuerID;
-    public String globallID;
+    public String globalID;
     public String issuerName;
     public String taxPayerID;
     public String issuerPostCode;
--- src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/IssuerTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group5/etaxinvoice/models/IssuerTest.java
@@ -6,25 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IssuerTest {
 
     private String issuerID = "PQRST12345";
-    private String globallID = "ABC123";
+    private String globalID = "ABC123";
     private String issuerName = "Natcha";
     private String taxPayerID = "123456789012300000";
     private String issuerPostCode = "10200";
-    
+
     @Test
     public void getValueFromIssuerModel() {
 
         Issuer.Builder builder = new Issuer.Builder();
         Issuer issuer = builder
                 .issuerID(this.issuerID)
-                .globallID(this.globallID)
+                .globalID(this.globalID)
                 .issuerName(this.issuerName)
                 .taxPayerID(this.taxPayerID)
                 .issuerPostCode(this.issuerPostCode)
                 .build();
 
         assertEquals(this.issuerID, issuer.issuerID);
-        assertEquals(this.globallID, issuer.globallID);
+        assertEquals(this.globalID, issuer.globalID);
         assertEquals(this.issuerName, issuer.issuerName);
         assertEquals(this.taxPayerID, issuer.taxPayerID);
         assertEquals(this.issuerPostCode, issuer.issuerPostCode);
